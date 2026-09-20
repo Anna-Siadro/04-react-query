c66e75fabdd6141da1458ae40b541e8b8308ff8a
Anna Siadro <anna_anna_111@ukr.net>
2026-09-20 17:12:15 +0300

    fix: move FetchMoviesResponse type to movieService and use axios generic

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,14 +1,21 @@
-import axios, { type AxiosResponse } from "axios";
-import type { FetchMoviesResponse } from "../types/movie";
+import axios from "axios";
+import type { Movie } from "../types/movie";
 
 const BASE_URL = "https://api.themoviedb.org/3";
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN as string;
+
+interface FetchMoviesResponse {
+  results: Movie[];
+  page: number;
+  total_pages: number;
+  total_results: number;
+}
 
 export const fetchMovies = async (
   query: string,
   page: number,
 ): Promise<FetchMoviesResponse> => {
-  const response: AxiosResponse<FetchMoviesResponse> = await axios.get(
+  const response = await axios.get<FetchMoviesResponse>(
     `${BASE_URL}/search/movie`,
     {
       params: { query, page },
